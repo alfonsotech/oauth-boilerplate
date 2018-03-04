bcf33b2803b133189b1fcfac6a0c5ad5adf71757
alfonsotech <alfonsotech@nym.hush.com>
2018-03-04 14:11:04 -0600

    adds user models, creates user from google login strategy

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const authRoutes = require('./routes/auth-routes')
 const passportSetup = require('./config/passport-setup')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 const PORT = 5000
 
 app.set('view engine', 'ejs')
+
+//connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log('Connected to MongoDB.');
+})
 
 //set up routes
 app.use('/auth', authRoutes)
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
+const User = require('../models/user-model')
 
 passport.use(
     new GoogleStrategy({
@@ -9,8 +10,21 @@ passport.use(
     clientID:keys.google.clientID,
     clientSecret:keys.google.clientSecret
   }, (accessToken, refreshToken, profile, done) => {
-    //passport callback function
-    console.log('passport callback function')
-    console.log('profile', profile);
+    //does user already exist in DB?
+    User.findOne({googleId: profile.id}).then( (currentUser) => {
+      if(currentUser){
+        //user already exists
+        console.log('Current user is: ', currentUser);
+      } else {
+        //create new user in DB
+        new User({
+          username: profile.displayName,
+          googleId: profile.id
+        }).save()
+        .then( (newUser) => {
+          console.log('New user saved: ', newUser);
+        })
+      }
+    })
   })
 )
